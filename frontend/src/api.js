8ecfa4c5e2a9a7e08f4ef949b6d007d8b6aaac6a
RIDapollo <112528695+RIDapollo@users.noreply.github.com>
2025-10-28 03:40:28 +0900

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,5 @@
 // 아아악 시이발 왜 이렇게 로컬호스트로 보내는거야 시발
 
-import axios from "axios";
+const API_URL = "https://g-pro-1104-backend.onrender.com";
 
-// NODE_ENV는 Vercel이 'npm run build'를 실행할 때 자동으로 'production'이 됩니다.
-const API_URL = process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_URL // Vercel 환경변수 (Render URL)
-    : "http://localhost:5000";        // 로컬 개발 환경
-
-axios.defaults.baseURL = API_URL;
-
-export default axios;
+export default API_URL;
